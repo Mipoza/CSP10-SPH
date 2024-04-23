7523e24882ff0b6a84aeed8cbe8629d9c0d09b00
Vincent Boulard <vboulard07@gmail.com>
2024-04-23 11:56:45 +0200

    Added code from Andres

--- sph_ippl/SPHManager.hpp
+++ sph_ippl/SPHManager.hpp
@@ -4,6 +4,13 @@
 // #pragma once
 // #include <iostream>
 
+
+template<unsigned DIM, unsigned N>
+unsigned getdim(Vector<Vector<double, DIM>,N>)
+{
+    return N;
+}
+
 // T: scalar, DIM: dimension, PC: Particle-Class
 template <typename T, unsigned DIM, class KERNEL = CubicSplineKernel<T, DIM>>
 struct SPHManager : public ippl::BaseManager {
@@ -12,7 +19,43 @@ struct SPHManager : public ippl::BaseManager {
   // Time-step size
   T dt;
   
-  SPHManager(SPHParticle<T, DIM, KERNEL>& P, double dt_): dt(dt_), particles(P) {}
+  SPHManager(SPHParticle<T, DIM, KERNEL>& P, T dt_): dt(dt_), particles(P) {}
+
+  void pre_run(Vector<Vector<T, DIM>,N> R_part, Vector<Vector<T, DIM>,N> v_part) 
+  {
+        unsigned N_new = getdim(R_part);
+        particles.create(N_new);
+
+        typename Manager<DIM, N>::particle_position_type::HostMirror R_host = particles.R.getHostMirror();
+        typename Manager<DIM, N>::particle_position_type::HostMirror v_host = particles.vel.getHostMirror();
+
+        for (unsigned int i = 0; i < N_new; ++i) {
+
+            R_host(i) = R_part[i];
+            v_host(i) = v_part[i];
+
+        }
+        Kokkos::deep_copy(particles.R.getView(), R_host);
+        Kokkos::deep_copy(particles.vel.getView(), v_host);
+
+        particles.update();
+  }
+
+  //As a preliminary way we pass the function as an argument of pre_step()
+  void pre_step(Vector<T, DIM> (*func)(Vector<T, DIM>)) 
+  { 
+      //particles.acceleration_function_external(func);
+  }
+
+  /**
+  * @brief A method that should be used after perfoming a step of simulation.
+  *
+  * Derived classes can override this method to dump data, increment time, etc.
+  * The default implementation does nothing.
+  */
+  void post_step() 
+  { cout << "post_step" << endl;}
+
 
   // Leap-Frog time-integration
   void advance() override {
